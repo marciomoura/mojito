aa505c017d7a2dbc2c9a4fe0b79f2ae546c43416
marciomoura <marciomoura.bridon@gmail.com>
2026-04-23 20:23:34 +0200

    feat: use modern cmake file sets and system dependencies (#10)

--- cmake/dependencies/gtest.cmake
+++ cmake/dependencies/gtest.cmake
@@ -7,24 +7,13 @@ set(FETCHCONTENT_QUIET ON)
 fetchcontent_declare(
     googletest
     URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
-        EXCLUDE_FROM_ALL)
+    SYSTEM
+    EXCLUDE_FROM_ALL)
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
-set(gtest_force_shared_crt
-    ON
-    CACHE BOOL "" FORCE)
-
-# Suppress warnings from GoogleTest
-set(OLD_WARN_DEPRECATED ${CMAKE_WARN_DEPRECATED})
-set(CMAKE_WARN_DEPRECATED
-    OFF
-    CACHE BOOL "" FORCE)
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 fetchcontent_makeavailable(googletest)
-
-set(CMAKE_WARN_DEPRECATED
-    ${OLD_WARN_DEPRECATED}
-    CACHE BOOL "" FORCE)
 
 if(NOT TARGET gtest::gtest)
     add_library(gtest::gtest ALIAS gtest)
