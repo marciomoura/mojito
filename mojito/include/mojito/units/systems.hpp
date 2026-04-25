f1aabaf0ad9ce3d187b828d3b2755a59a5a1b25d
Marcio <marciomoura.bridon@gmail.com>
2026-04-25 11:52:06 +0200

    feat: support custom per-unit bases with explicit templated aliases

--- mojito/include/mojito/units/systems.hpp
+++ mojito/include/mojito/units/systems.hpp
@@ -1,9 +1,27 @@
 #pragma once
 
+#include <type_traits>
+
 namespace mojito {
 
 struct si {};
-struct per_unit {};
+
+struct default_base {};
+
+template <typename Base = default_base>
+struct per_unit {
+    using base = Base;
+};
+
 struct percent {};
+
+template <typename T>
+struct is_per_unit : std::false_type {};
+
+template <typename Base>
+struct is_per_unit<per_unit<Base>> : std::true_type {};
+
+template <typename T>
+inline constexpr bool is_per_unit_v = is_per_unit<T>::value;
 
 }  // namespace mojito
